ce0bf74a033eee84ae36c80ef32f3a9a8c5a9a17
shounakdatta <shounak14datta@gmail.com>
2019-09-08 12:38:35 -0400

    Fetch multiple products working

--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -2,23 +2,28 @@ import { firestore } from "../config/Fire";
 import { getUser } from "./UserService";
 import Store from "../store";
 
-export const getLastMonthPricesForAllProducts = () => {
-  const { dbid } = Store.getState().UserStore.toJS().userObj;
-  const userRef = firestore.doc(`users/${dbid}`);
-  userRef.get().then(doc => {
-    const { products } = doc.data();
-    let dateLastMonth = new Date();
-    dateLastMonth.setMonth(new Date().getMonth() - 1);
-    products.forEach(({ product, start }) => {
-      const startDate = start.toDate();
-      const latestDate = startDate > dateLastMonth ? startDate : dateLastMonth;
-      firestore
-        .doc(product.path)
-        .collection("prices")
-        .where("date", ">=", latestDate)
-        .get()
-        .then(snap => snap.forEach(doc => console.log(doc.data())))
-        .catch(err => console.log("Error", err));
+export const getLastMonthPrices = () => {
+  return new Promise((resolve, reject) => {
+    const { dbid } = Store.getState().UserStore.toJS().userObj;
+    const userRef = firestore.doc(`users/${dbid}`);
+    userRef.get().then(async doc => {
+      const { products } = doc.data();
+      let dateLastMonth = new Date();
+      dateLastMonth.setMonth(new Date().getMonth() - 1);
+      const prodData = await Promise.all(
+        products.map(async ({ product, start }) => {
+          const startDate = start.toDate();
+          const latestDate =
+            startDate > dateLastMonth ? startDate : dateLastMonth;
+          return await product
+            .collection("prices")
+            .where("date", ">=", latestDate)
+            .get()
+            .then(snap => snap.docs.map(doc => doc.data()))
+            .catch(({ code, message }) => reject({ errorCode: code, message }));
+        })
+      );
+      resolve(prodData);
     });
   });
 };
